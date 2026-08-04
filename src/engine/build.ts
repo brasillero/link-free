bb508fffe4841c53f7395fc8c6e66eadfd0f3e61
Luca A.R. <luca.alfaro.rampinelli@gmail.com>
2026-08-04 17:10:56 -0300

    feat: build pipeline copies local assets

--- src/engine/build.ts
+++ src/engine/build.ts
@@ -1,11 +1,13 @@
 import { mkdir, writeFile } from "node:fs/promises";
 import { join } from "node:path";
+import { processAssets } from "./assets.js";
 import { loadSections } from "./loadSections.js";
 import { renderPage } from "./renderPage.js";
 
 export async function build(dir: string, outDir: string): Promise<string> {
   const sections = await loadSections(dir);
-  const html = renderPage(sections);
+  const withAssets = await processAssets(sections, dir, outDir);
+  const html = renderPage(withAssets);
   await mkdir(outDir, { recursive: true });
   const outPath = join(outDir, "index.html");
   await writeFile(outPath, html, "utf8");
